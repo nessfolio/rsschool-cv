2e1607e96abe2f8de28930b5fa2b5d396ea4142e
ness <nessfolio@gmail.com>
2026-02-17 18:32:22 +0600

    fix: usage of nav

---
+++ script.js
@@ -0,0 +1,39 @@
+const anchors = document.querySelectorAll("a[href*='#']")
+// *= means contains
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        const anchorID = anchor.getAttribute('href')
+        const target = document.querySelector(anchorID)
+        
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'center'
+        })
+    })
+}
+
+
+
+
+const arrow = document.getElementById('arrow')
+const padding = document.getElementById('padding')
+
+let change = 'block'
+arrow.onclick = function() {
+    padding.style.display = change;
+    if (change === 'block') change = 'none';
+    else if (change === 'none') change = 'block';
+}
+
+
+window.addEventListener('resize', () => {
+    console.log('resize')
+    console.log(window.innerWidth)
+    if (window.innerWidth > 800) {
+        padding.style.display = 'none';
+        change = 'block';
+    }
+})
